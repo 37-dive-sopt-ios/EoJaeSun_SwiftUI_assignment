299990eeeaf27b9c28ba0761a371a19897783869
JaeSunEo <wotjs020708@gmail.com>
2025-12-08 01:26:58 +0900

    [init] 프로젝트 생성

---
+++ EoJaeSun_SwiftUI_assignment/EoJaeSun_SwiftUI_assignment/EoJaeSun_SwiftUI_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EoJaeSun_SwiftUI_assignmentApp.swift
+//  EoJaeSun_SwiftUI_assignment
+//
+//  Created by 어재선 on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EoJaeSun_SwiftUI_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
